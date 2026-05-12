5fe61ad4d599cda8759a5ae85e60c6d5dcb56195
Ngash <clarenchomba59@gmail.com>
2026-05-12 17:53:25 +0200

    done with writeline, int, float, string, concat

---
+++ csharp-hello_world/5-print_float/5-print_float.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double number = 3.14159;
+        Console.WriteLine("Float: " + number.ToString("0.00"));
+    }
+}
