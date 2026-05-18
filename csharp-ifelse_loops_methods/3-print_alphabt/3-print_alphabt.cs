efbaf56e32e3e025c9fdc0c05e8a8ae8e02ebc8f
Ngash <clarenchomba59@gmail.com>
2026-05-19 00:00:13 +0200

    alphabet without q and e

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            if (c != 'q' && c != 'e')
+            {
+                Console.Write(c);
+            }
+        }
+        Console.WriteLine();
+    }
+}
