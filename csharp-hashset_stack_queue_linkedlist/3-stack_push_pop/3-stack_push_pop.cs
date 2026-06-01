cdeb4a5840d313b92b36d1f9267d0a408517093d
Ngash <clarenchomba59@gmail.com>
2026-06-01 11:13:39 +0200

    prints stack info and removes items up to a search

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: " + aStack.Count);
+
+        if (aStack.Count == 0)
+        {
+            Console.WriteLine("Stack is empty");
+            aStack.Push(newItem);
+            return aStack;
+        }
+
+        Console.WriteLine("Top item: " + aStack.Peek());
+
+        bool contains = aStack.Contains(search);
+        Console.WriteLine("Stack contains \"" + search + "\": " + contains);
+
+        if (contains)
+        {
+            string[] arr = aStack.ToArray();
+            int idx = -1;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == search)
+                {
+                    idx = i;
+                    break;
+                }
+            }
+
+            aStack.Clear();
+            for (int i = arr.Length - 1; i > idx; i--)
+                aStack.Push(arr[i]);
+        }
+
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
