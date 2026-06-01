4a00590814e0000e988b5fbab47e9581ab4a120c
Ngash <clarenchomba59@gmail.com>
2026-06-01 11:15:23 +0200

    prints queue info and removes items up to a search

---
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: " + aQueue.Count);
+
+        if (aQueue.Count == 0)
+        {
+            Console.WriteLine("Queue is empty");
+            aQueue.Enqueue(newItem);
+            return aQueue;
+        }
+
+        Console.WriteLine("First item: " + aQueue.Peek());
+
+        aQueue.Enqueue(newItem);
+
+        bool contains = aQueue.Contains(search);
+        Console.WriteLine("Queue contains \"" + search + "\": " + contains);
+
+        if (contains)
+        {
+            string[] arr = aQueue.ToArray();
+            int idx = -1;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == search)
+                {
+                    idx = i;
+                    break;
+                }
+            }
+
+            aQueue.Clear();
+            for (int i = idx + 1; i < arr.Length; i++)
+                aQueue.Enqueue(arr[i]);
+        }
+
+        return aQueue;
+    }
+}
