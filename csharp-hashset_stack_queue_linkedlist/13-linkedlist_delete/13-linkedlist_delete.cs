9ff96efa3301811bccb85d94b2978e75d308f074
Ngash <clarenchomba59@gmail.com>
2026-06-01 11:39:05 +0200

    Delete the node at a given position in a linked list

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> current = myLList.First;
+        for (int i = 0; i < index && current != null; i++)
+            current = current.Next;
+
+        if (current != null)
+            myLList.Remove(current);
+    }
+}
