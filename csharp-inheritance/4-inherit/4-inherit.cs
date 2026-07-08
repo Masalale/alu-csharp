070ad9bc7864a1714c2b10db7c040b0459a95cf8
Ngash <clarenchomba59@gmail.com>
2026-07-08 12:37:15 +0200

    adds Dog class inheriting from Animal

---
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,15 @@
+using System;
+
+/// <summary>
+/// Represents an animal.
+/// </summary>
+class Animal
+{
+}
+
+/// <summary>
+/// Represents a dog, inheriting from Animal.
+/// </summary>
+class Dog : Animal
+{
+}
